4463e8ea76a85cb9fe30cf5d22656af7c5b08ec8
Tyler Slonecki <sloneckity@gmail.com>
2025-06-16 14:56:44 -0400

    Deployment check

--- microhapDB-frontend/terraform/variables.tf
+++ microhapDB-frontend/terraform/variables.tf
@@ -3,13 +3,13 @@
 variable "aws_region" {
   description = "AWS region for resources"
   type        = string
-  default     = "us-east-2"
+  default     = "us-east-1"
 }
 
 variable "project_name" {
   description = "Name of the project (used for resource naming)"
   type        = string
-  default     = "microhapdb"
+  default     = "haplosearch"
 }
 
 variable "environment" {
